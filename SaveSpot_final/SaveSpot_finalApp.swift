4f80077195721483d2051adc8a7f3eb388c5e788
Ryota-111 <taismryotasis@gmail.com>
2024-07-12 15:10:50 +0900

    Initial Commit

---
+++ SaveSpot_final/SaveSpot_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveSpot_finalApp.swift
+//  SaveSpot_final
+//
+//  Created by Ryota Fujitsuka on 2024/07/12.
+//
+
+import SwiftUI
+
+@main
+struct SaveSpot_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
